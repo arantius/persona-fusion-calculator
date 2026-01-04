4ae14b987b974d5edb3e4855473f47638c81abfc
JacobLKZ <jacoblimkz@gmail.com>
2026-01-04 11:06:36 +0800

    Replaced reference link

--- data4golden.js
+++ data4golden.js
@@ -1,7 +1,5 @@
 // Derived from:
-//   http://www.gamefaqs.com/ps2/945498-shin-megami-tensei-persona-4/faqs/54981
-//   http://www.gamefaqs.com/ps2/945498-shin-megami-tensei-persona-4/faqs/54981
-//   http://db.gamefaqs.com//portable/vita//file/persona_4_golden_fusion.png
+//   https://aqiu384.github.io/megaten-fusion-tool/p4g/chart
 
 const personae = [
     {'arcana': 'Fool',        'level':  1, 'name': 'Izanagi',           },
